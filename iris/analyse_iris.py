3c1b9cbee00ae6cd4730630860fcc4b46858adff
Tim Schäfer <ts+code@rcmd.org>
2019-06-08 17:48:23 +0200

    add iris dataset

---
+++ iris/analyse_iris.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+import pandas as pd
+
+
+def analysis():
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    dataset = pandas.read_csv(url, names=names)
+
+if __name__ == "__main__":
+    analysis()

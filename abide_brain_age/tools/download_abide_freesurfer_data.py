bdf0ca3a6b249ec4fb617e8a12e5ce90e71c38c5
Tim Schäfer <ts+code@rcmd.org>
2019-07-08 11:32:19 +0200

    also grab labels and pial morphometry data in native space

--- abide_brain_age/tools/download_abide_freesurfer_data.py
+++ abide_brain_age/tools/download_abide_freesurfer_data.py
@@ -2,6 +2,8 @@
 #
 # This script downloads all ABIDE I structural neuroimaging data, preprocessed using the FreeSurfer pipeline.
 #
+# This is a parallel implementation. Sequential takes ages.
+#
 # Written by Tim Schäfer, 2019-06-05
 
 
@@ -13,7 +15,6 @@ import shutil
 import errno
 from multiprocessing.dummy import Pool
 from urllib.request import urlretrieve
-# Download the file from `url` and save it locally under `file_name`:
 from os.path import expanduser
 
 def get_abide():
@@ -22,26 +23,28 @@ def get_abide():
     #required_files_relative_to_subject_dir = ["surf/lh.white", "surf/rh.white", "surf/notthere"]
     required_files_relative_to_subject_dir = get_fs_subject_filenames()
 
-    for file_idx, file in enumerate(required_files_relative_to_subject_dir):
-        print("%d: %s" % (file_idx+1, file))
-
     #diagnose_output("............................................ssssss......ssssss......ssssss......ssssss......ssssss......ssssss", required_files_relative_to_subject_dir)
 
-    print("Downloading ABIDE I structural data to local directory '%s'." % (local_base_dir))
-    print("Will download %d files per subject." % (len(required_files_relative_to_subject_dir)))
+    print("Downloading ABIDE I structural data to local directory '%s'." % (local_base_dir), flush=True)
+    print("Will download %d files per subject:" % (len(required_files_relative_to_subject_dir)), flush=True)
+    for file_idx, file in enumerate(required_files_relative_to_subject_dir):
+        print("    %d: %s" % (file_idx+1, file))
 
-    download_abide_structural_files_to(local_base_dir, required_files_relative_to_subject_dir, skip_existing=True)
+
+    download_abide_structural_files_to(local_base_dir, required_files_relative_to_subject_dir, skip_existing=True, num_in_parallel = 12)
 
 
 def get_fs_subject_filenames():
     """Generates the file paths of all important FreeSurfer files for a subject, relative to its subject dir."""
     files = []
-    files = files + _get_both_hemi_files_in_dir("surf", ["white", "pial", "inflated", "orig", "smoothwm", "sphere", "sphere.reg", "jacobian_white", "thickness", "area", "curv", "volume", "sulc"])
-    files = files + _get_both_hemi_files_in_dir("stats", ["aparc.stats", "aparc.a2009s.stats"])
-    files = files + _get_files_in_subdir("stats", ["aseg.stats"])
-    files = files + _get_files_in_subdir("mri", ["brain.mgz", "brainmask.mgz", "orig.mgz", "T1.mgz", "aseg.mgz"])
-    files = files + _get_files_in_subdir("mri/transforms", ["talairach.m3z", "talairach.xfm"])
-    files = files + _get_surf_both_hemi_fsaverge_mappings(["area", "area.pial", "sulc", "thickness", "curv", "volume"])
+    files = files + _get_both_hemi_files_in_dir("surf", ["white", "pial", "inflated", "orig", "smoothwm", "sphere", "sphere.reg"])      # surfaces
+    files = files + _get_both_hemi_files_in_dir("surf", ["jacobian_white", "thickness", "area", "area.pial", "curv", "curv.pial", "volume", "sulc"])        # surface morphometry data (native space)
+    files = files + _get_surf_both_hemi_fsaverge_mappings(["area", "area.pial", "sulc", "thickness", "curv", "curv.pial", "volume"])     # surface morphometry data mapped to standard space (fsaverage surface)
+    files = files + _get_both_hemi_files_in_dir("stats", ["aparc.stats", "aparc.a2009s.stats"])     # atlas stats. Note: "aparc.DKTatlas.stats" is not available for ABIDE
+    files = files + _get_files_in_subdir("stats", ["aseg.stats"])   # segmenatation stats, not hemi dependent
+    files = files + _get_both_hemi_files_in_dir("label", ["aparc.annot", "aparc.a2009s.annot", "cortex.label"])     # brain surface parcellations for standard atlases and other labels
+    files = files + _get_files_in_subdir("mri", ["brain.mgz", "brainmask.mgz", "orig.mgz", "T1.mgz", "aseg.mgz"])   # volumes
+    files = files + _get_files_in_subdir("mri/transforms", ["talairach.m3z", "talairach.xfm"])  # transformation matrices to convert between different spaces
     return files
 
 
@@ -61,20 +64,20 @@ def _get_files_in_subdir(subdir, file_list):
     return [("%s/%s" % (subdir, f)) for f in file_list]
 
 
-def _get_surf_both_hemi_fsaverge_mappings(file_name_parts_list, subdir="surf"):
+def _get_surf_both_hemi_fsaverge_mappings(file_name_parts_list, subdir="surf", smoothings=["0", "5", "10", "15", "20", "25"]):
     """
     Supply something like ["area", "volume"] to get a list of file names including ["lh.area.fwhm0.fsaverage.mgh", "lh.area.fwhm0.fsaverage.mgh", lh.area.fwhm5.fsaverage.mgh, ...]
     """
     fsaverage_parts = []
     files = []
-    for smoothing in ["0", "5", "10", "15", "20", "25"]:
+    for smoothing in smoothings:
         files_lh = [("%s/lh.%s.fwhm%s.fsaverage.mgh" % (subdir, f, smoothing)) for f in file_name_parts_list]
         files_rh = [("%s/lh.%s.fwhm%s.fsaverage.mgh" % (subdir, f, smoothing)) for f in file_name_parts_list]
         files = files + files_lh + files_rh
     return files
 
 
-def download_abide_structural_files_to(local_base_dir, required_files_relative_to_subject_dir, verbose=True, skip_existing=False):
+def download_abide_structural_files_to(local_base_dir, required_files_relative_to_subject_dir, verbose=True, skip_existing=False, num_in_parallel = 8):
     if not os.path.exists(local_base_dir):
         raise IOError("Local base directory '%s' does not exist, please create it first." % (local_base_dir))
     md = load_abide_metadata()
@@ -83,9 +86,9 @@ def download_abide_structural_files_to(local_base_dir, required_files_relative_t
     #file_ids = pd.Series(["Pitt_0050003", "Pitt_0050004", "Pitt_0050005"])
 
     num_ids = len(file_ids)
-    num_files_total = (num_ids * len(required_files_relative_to_subject_dir))
+    num_files_total_max = (num_ids * len(required_files_relative_to_subject_dir))
     if verbose:
-        print("Received meta data on %d subjects. Will download %d * %d = %d files in total." % (num_ids, num_ids, len(required_files_relative_to_subject_dir), num_files_total))
+        print("Received meta data on %d subjects. May download up to %d * %d = %d files in total (if all requested files are available for all subjects)." % (num_ids, num_ids, len(required_files_relative_to_subject_dir), num_files_total_max))
 
     base_url = "https://s3.amazonaws.com/fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/"
     ok_files = dict()
@@ -94,41 +97,39 @@ def download_abide_structural_files_to(local_base_dir, required_files_relative_t
 
     url_tuples = list()
 
-    num_in_parallel = 8
+    num_no_filename_files = 0
 
     for idx, fid in file_ids.items():
         if fid == "no_filename":
+            num_no_filename_files = num_no_filename_files + len(required_files_relative_to_subject_dir)
             continue
-        for fpath in required_files_relative_to_subject_dir:
-            fpath = fid + "/" + fpath
-            fparts = fpath.split("/")
-            extra_dirs = []
-            file_name = fparts[-1]
-            if len(fparts) > 1:
-                extra_dirs = fparts[0:-1]
-            url_tuple = (base_url, extra_dirs, file_name, local_base_dir, skip_existing, ok_files, error_files, skipped_files, verbose)
-            url_tuples.append(url_tuple)
+        else:
+            for fpath in required_files_relative_to_subject_dir:
+                fpath = fid + "/" + fpath
+                fparts = fpath.split("/")
+                extra_dirs = []
+                file_name = fparts[-1]
+                if len(fparts) > 1:
+                    extra_dirs = fparts[0:-1]
+                url_tuple = (base_url, extra_dirs, file_name, local_base_dir, skip_existing, ok_files, error_files, skipped_files, verbose)
+                url_tuples.append(url_tuple)
 
         if verbose:
             print("")
 
+    num_files_total = len(url_tuples)
     print("Downloading %d files in parallel using %d threads." % (num_files_total, num_in_parallel))
-    Pool(num_in_parallel).map(retrieve_url, url_tuples)
-    print("Download finished.")
 
-    if len(error_files.keys()):
-        print("Encountered problems with %d of the %d files:" % (len(error_files.keys()), num_files_total))
-        for k in error_files.keys():
-            print("-error url '%s' : '%s'" % (k, error_files[k]))
-    if len(skipped_files.keys()):
-        print("Skipped %d of the %d files:" % (len(skipped_files.keys()), num_files_total))
-        for k in skipped_files.keys():
-            print("-skipped file %s" % (k, skipped_files[k]))
-    print("All done: handled %s URLs in total (%d in parallel). Result: %d OK, %d skipped, %d failed." % (num_files_total, num_in_parallel, len(ok_files.keys()), len(skipped_files.keys()), len(error_files.keys())))
+    Pool(num_in_parallel).map(retrieve_url, url_tuples)
+
+    print("Download finished: handled %s URLs in total (%d in parallel). Result: %d downloaded, %d existed, %d failed, %d no_filename." % (num_files_total, num_in_parallel, len(ok_files.keys()), len(skipped_files.keys()), len(error_files.keys()), num_no_filename_files))
     print("Check local directory '%s' for downloaded files. Exiting." % (local_base_dir))
 
 
 def retrieve_url(arg_tuple):
+    """
+    The actual downloader.
+    """
     base_url, extra_dirs, file_name, local_base_dir, skip_existing, ok_files, error_files, skipped_files, verbose = arg_tuple
     code, full_url, msg = download_file_to_local_dir(base_url, extra_dirs, file_name, local_base_dir, skip_existing=skip_existing)
 
@@ -146,6 +147,11 @@ def retrieve_url(arg_tuple):
     else:
         error_files[full_url] = "Unhandled exception: '%s'" % (msg)
         print("EE: %s" % (full_url), flush=True)
+
+    if verbose:
+        num_so_far = len(ok_files) + len(error_files) + len(skipped_files)
+        if num_so_far % 1000 == 0:
+            print("# %d" % (num_so_far))
 
 
 def download_file_to_local_dir(base_url, extra_dirs, file_name, local_base_dir, skip_existing=False):

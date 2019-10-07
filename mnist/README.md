3f71525159765b15efdc2259206e6644767b6f92
Tim Schäfer <ts+code@rcmd.org>
2019-10-07 09:15:23 +0200

    Add MNIST solution

---
+++ mnist/README.md
@@ -0,0 +1,1 @@
+Keras code to run a not-so-deep neural network on the famous MNIST handwritten digits dataset.
---
+++ mnist/mnist_tensorflow_keras.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0    # scale image intensities to range 0..1
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(256, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+callbacks = [tf.keras.callbacks.TensorBoard(log_dir='./logs', profile_batch=0)]
+
+hist = model.fit(x_train, y_train, epochs=10, callbacks=callbacks)
+model.evaluate(x_test, y_test)
+
+
+print(hist.history)
